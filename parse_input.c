fd061f15a9b40c286a5e29505b298daf718c820c
jsantos <jsantos@student.42lisboa.com>
2022-04-22 16:52:23 +0100

    Doing the parser.

--- parse_input.c
+++ parse_input.c
@@ -1,6 +1,17 @@
 #include "parse_input.h"
 
 int	parse_input(t_cmds* cmds) {
+	int i;
+
+	i = -1;
+	while (cmds->input[++i] != '\0')
+	{
+		//skip_white_space
+		if (cmds->input[i] == '|')
+			;//handle_pipe()
+		else if (cmds->input[i] == ';')
+			;//handle_semicolon()
+	}
 
 
 }
